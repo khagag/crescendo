eb86a1cb3e8c7ed5bc2e86c9de9625a0a216b8fb
khagag <the_lord29@yahoo.com>
2019-06-03 05:39:53 +0200

    regular save

--- api/views.py
+++ api/views.py
@@ -13,28 +13,29 @@ from dejavu.recognize import FileRecognizer
 from django.http import HttpResponse
 config = {
         "database" :{
-        "host":"127.0.0.1",
+            "host":"127.0.0.1",
         "user": "root",
         "passwd": "kemo",
-        "db": "dejavu"
+        "db": "crescendo"
         }
     }
 
 
-div=Dejavu(config)
+djv=Dejavu(config)
 
 from dejavu.recognize import FileRecognizer
 @api_view(["GET","POST"])
 def index(request):
-    #song = djv.recognize(FileRecognizer,)
+    song = djv.recognize(FileRecognizer,)
     """if request.method == 'GET':
         print("get request")
     elif request.method == 'POST':
         print("post request")
         """
     if request.method == 'POST':
-        path = default_storage.save('/home/poor_one/django_training/ch_app/file', request.FILES['song'])
-        song=div.recognize(FileRecognizer,path)
+        pass
+        #path = default_storage.save('/home/poor_one/django_training/ch_app/file', request.FILES['song'])
+        #song=div.recognize(FileRecognizer,path)
 
         return Response(song)
     return HttpResponse(request.method) 
--- crescendo/models.py
+++ crescendo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-
+class Comboser(models.Model):
+    FName = models.CharField(max_length=50)
+    MName = models.CharField(max_length=50)
+    LName = models.CharField(max_length=50)
+    BirthDate = models.DateField()
